a1661b5a7d7d7771d85c370417f5498f946a6325
Animesh Jain <anijain@umich.edu>
2024-10-10 23:18:50 -0700

    [hierarchical-compilation][inductor] Lower invoke_subgraph
    ghstack-source-id: c2df1c594b1adbda9fd3363fe9678f164e1fc982
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/137618

--- test/higher_order_ops/test_invoke_subgraph.py
+++ test/higher_order_ops/test_invoke_subgraph.py
@@ -173,7 +173,7 @@ class TestInvokeSubgraphCompile(TestCase):
 
         x_clone = x.clone().detach().requires_grad_(True)
         y_clone = y.clone().detach().requires_grad_(True)
-        res = torch.compile(fn, backend="aot_eager")(x_clone, y_clone)
+        res = torch.compile(fn, backend="inductor")(x_clone, y_clone)
 
         # Run backward
         ref.sum().backward()
@@ -237,7 +237,7 @@ class TestInvokeSubgraphCompile(TestCase):
 
         x_clone = x.clone().detach().requires_grad_(True)
         y_clone = y.clone().detach().requires_grad_(True)
-        res = torch.compile(fn, backend="aot_eager")(x_clone, y_clone)
+        res = torch.compile(fn, backend="inductor")(x_clone, y_clone)
 
         # Run backward
         ref.sum().backward()
